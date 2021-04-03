177227e6182acef2708f1d7218240119686c3de3
Welisson luca <welissonluca17@gmail.com>
2021-04-03 16:03:47 -0300

    chore:  commit-msg-linter add

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 const express = require('express');
 
-const visitorsRouter = require("./routes/visitors.routes");
-
-const visitRouter = require("./routes/visit.routes");
+const visitorsRouter = require("./routes/visitors.routes"); // Rota dos visitantes
+const visitRouter = require("./routes/visit.routes"); // Rotas das visitas
 const app = express();
+
 
 app.use(express.json());
 
+app.use("/", visitorsRouter); 
 
-app.use("/", visitorsRouter);
 app.use("/", visitRouter);  
 
 
 
 app.listen(3000, () => console.log('server is running'))
+
+

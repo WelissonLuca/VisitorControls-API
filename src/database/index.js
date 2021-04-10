b01e0aee81b54ca9c0b63cd631d5b5cf5ec857a5
Thales <thales.c.santana@outlook.com>
2021-04-10 11:06:05 -0300

    fix: try to fix dotenv bug

--- src/database/index.js
+++ src/database/index.js
@@ -1,23 +1,8 @@
 const Sequelize = require('sequelize')
-const dbConfig =  require('../config/database')
-
+const configDB = require('../config/database')
 const Visitantes = require('../Models/Visitantes')
 
-const connection = new Sequelize(
-    process.env.DB_DATABASE,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-        host: process.env.DB_HOST,
-        dialect:"mysql"
-    },
-);
-connection({
-    define: {
-        timestamps: true, //define BD sempre criar created_at updated_at
-        underscored: true, // define padrão do BD como snake_case
-    }
-})
+const connection = new Sequelize(configDB)
 Visitantes.init(connection)
 
 module.exports = connection;

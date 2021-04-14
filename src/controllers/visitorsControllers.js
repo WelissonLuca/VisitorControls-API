8ac760014b401072a6dfe22cca4869fbabaca7f6
Welisson luca <welissonluca17@gmail.com>
2021-04-14 16:53:07 -0300

    fix: visitors already exists verify fixed

--- src/controllers/visitorsControllers.js
+++ src/controllers/visitorsControllers.js
@@ -1,24 +1,29 @@
 const Visitors = require("../Models/Visitantes");
 const dictionary = require("../utils/dictionary");
-const validations = require("../utils/validations");
+
 
 module.exports = {
 	async store(req, res) {
 		try {
 			const { nome, rg, tipo } = req.body;
-			validations.userAlreadyExists(rg,res)
+			const visitor = await Visitors.findOne({ where: { rg } });
+			if (visitor != undefined)
+				return res
+					.status(dictionary.status.BAD_REQUEST)
+					.send(dictionary.messages.USUARIO_EXISTS);
 			const visitors = await Visitors.create({
 				nome,
 				rg,
 				tipo,
 			});
-			return res
-				.status(dictionary.status.CREATED)
-				.send(dictionary.messages.USER_CREATED);
+			return res.status(dictionary.status.CREATED).json({
+				message: dictionary.messages.USER_CREATED,
+				visitors,
+			});
 		} catch (err) {
 			return res
 				.status(dictionary.status.BAD_REQUEST)
-				.send({ error: err.messages });
+				.send({error: err});
 		}
 	},
 };

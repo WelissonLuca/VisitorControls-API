0632ce201b965886cdaa923cad86e3ff803dcbe7
rafael-albano <rafael.albano88@gmail.com>
2021-04-17 11:03:04 -0300

    feat: create-route-update-visitorcontroller

--- src/routes/visitors.routes.js
+++ src/routes/visitors.routes.js
@@ -5,5 +5,8 @@ const visitorsRouter = express.Router();
 visitorsRouter.post("/visitors",visitorsControllers.store);
 
 visitorsRouter.get("/visitors", visitorsControllers.list);
+visitorsRouter.put("/visitors", visitorsControllers.update);
+
+
 
 module.exports = visitorsRouter;
